ed2bbe49b900f1deaa9db0341ba2a9cf9b249344
jonayeds <jonayeeds@gmail.com>
2024-11-11 21:23:23 +0600

    added navlink anumation

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,19 +1,45 @@
+import {gsap} from "gsap"
+
 
 const NavBar = () => {
+    const handleNavAnimation = ()=>{
+        gsap.to(".navLink",{
+            display:"flex",
+            width:62,
+            duration:1,
+
+        })
+    }
+    const navBarOutAnimation = ()=>{
+        gsap.to(".navLink", {
+            width:0,
+            duration:1,
+        })
+    }
+
   return (
     <nav className="relative w-screen -mx-[7vw] " >
-        <div className="flex w-full items-center px-[7vw] fixed justify-between py-4  bg-gray-300">
+        <div className="flex w-full items-center px-[7vw] fixed justify-between py-4  ">
             <div>
                 <h1 className="text-5xl">Logo</h1>
             </div>
             <div>
-                <ul className="flex gap-[2vw]">
-                    <li>Discover</li>
+                <ul className="flex gap-[2vw] font-semibold text-gray-700">
+                    <li>
+                        <div className="relative " onMouseOver={handleNavAnimation}>
+                            <p>Discover</p>
+                            <div className="w-0 absolute navLink hidden overflow-hidden mt-2">
+                            <svg  height={"25"} className="" width={"62"}>
+                            <path d="M 0 0 C 20 20, 40 20, 62 0"  stroke="black" style={{strokeWidth:"2px"}}  fill="transparent"/>
+                            </svg>
+                            </div>
+                        </div>
+                    </li>
                     <li>Contacts</li>
                 </ul>
             </div>
             <div>
-                <button>Get Started</button>
+                <button className="bg-black px-6 text-center py-2 text-white rounded-full" >Get Started</button>
             </div>
         </div>
     </nav>
