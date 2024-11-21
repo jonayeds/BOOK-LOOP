8f5924fc06b336cfb14b79974a28361852876a9d
jonayeds <jonayeeds@gmail.com>
2024-11-21 16:03:00 +0600

    added animation to change background color

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,15 +2,37 @@
 import HomeSubHeading from "./HomeSubHeading"
 import HomeImagination from "./HomeImagination"
 import HomeHowToGuide from "./HomeHowToGuide"
-
-
+import HomeHeroSection from "./HomeHeroSection"
+import { gsap } from "gsap"
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+gsap.registerPlugin(useGSAP, ScrollTrigger)
 const Home = () => {
-
+  useGSAP(()=>{
+    const bgChangeTimeLine = gsap.timeline({
+      scrollTrigger:{
+        trigger:".homeContainer",
+        start:"center top",
+        end:"bottom bottom",
+        markers:false,
+        scrub:1,
+      }
+    })
+    bgChangeTimeLine
+    .fromTo(".homeContainer",{
+      backgroundColor:"#F1F2ED"
+    },{
+      backgroundColor:"#d9e6ee"
+    })
+  })
   return (
-    <div className="bg-[#F1F2ED]   " >
-      <div className="  mx-auto  font-heading text-dark max-w-[93vw] " >
+    <div className="homeContainer" >
+      <div className="    px-[3.5vw]  font-heading text-dark  " >
+        <HomeHeroSection />
         <HomeSubHeading />
         <HomeImagination />
+      </div>
+      <div className=" min-h-screen">
         <HomeHowToGuide />
       </div>
     </div>
--- client/src/pages/Home/HomeHowToGuide.jsx
+++ client/src/pages/Home/HomeHowToGuide.jsx
@@ -1,7 +1,7 @@
 
 const HomeHowToGuide = () => {
   return (
-    <div className="bg-white">
+    <div className="">
         <h2 className="uppercase text-[2.5vw] text-center">How it works</h2>
     </div>
   )
